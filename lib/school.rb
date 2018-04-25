f7ef09ae513235691da3e1c7cdfcb2301ce24eb6
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-25 15:47:37 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,8 +3,8 @@ class School
   def initialize(school)
     @school = school
   end
-  #
-  # def roster=(roster)
-  #   @roster = roster
-  # end
+  
+  def roster=(roster)
+    @roster = roster
+  end
 end
